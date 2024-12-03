4855598060ad6e6bcc500a0916260b6cf0982fff
OmarHiger <ohiger2015@gmail.com>
2024-12-03 19:15:09 +0300

    feat: add auth middlware in task routes

--- routes/task.php
+++ routes/task.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(TaskController::class)->prefix('tasks')->group(function ($router) {
     Route::get('/', 'index');
     Route::get('/{id}', 'show');
-    Route::post('/', 'store');
-    Route::put('/{id}', 'update');
-    Route::delete('/{id}', 'destroy');
+    Route::post('/', 'store')->middleware('auth:sanctum');
+    Route::put('/{id}', 'update')->middleware('auth:sanctum');
+    Route::delete('/{id}', 'destroy')->middleware('auth:sanctum');
 });
